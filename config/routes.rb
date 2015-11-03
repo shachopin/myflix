a08adb70c40eb8290847da04e6faca72037f77c4
Dawei Dai <shachopin@gmail.com>
2015-11-03 08:15:02 -0800

    queue related features

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Myflix::Application.routes.draw do
     resources :reviews, only:[:create]
   end
   #get '/videos/:id', to: "videos#show", as: "video"
+  resources :queue_items, only: [:create, :destroy]
+  get '/my_queue', to: "queue_items#index"
   get '/categories/:id', to: "categories#show", as: "category"
   get '/register', to: "users#new"
   get '/sign_in', to: "sessions#new"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-category1 = Category.create!(name: "Romantic Comedy");
-category2 = Category.create!(name: "Action Thriller");
+category1 = Category.create!(name: "Cartoon");
+category2 = Category.create!(name: "Comedy");
 
-Video.create!(title: "Deep Impact", description: "Wow, a gret movie", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Air Force 1", description: "Stunning", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Notebook", description: "I cried", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-yesman = Video.create!(title: "Yes Man", description: "Must see", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Deep Impact", description: "Wow, a gret movie", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Air Force 1", description: "Stunning", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Notebook", description: "I cried", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Yes Man", description: "Must see", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Deep Impact", description: "Wow, a gret movie", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Air Force 1", description: "Stunning", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Notebook", description: "I cried", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Yes Man", description: "Must see", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Deep Impact", description: "Wow, a gret movie", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Air Force 1", description: "Stunning", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
-Video.create!(title: "Notebook", description: "I cried", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
-Video.create!(title: "Yes Man", description: "Must see", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category1)
+monk = Video.create!(title: "Monk", description: "Wow, a gret movie", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: category2)
+family_guy = Video.create!(title: "family_guy", description: "Stunning", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/family_guy.jpg", category: category1)
+south_park = Video.create!(title: "South Park", description: "Must see", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg", category: category2)
+futurama = Video.create!(title: "Futurama", description: "So Funny", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: category1)
 
 dawei = User.create!(email: "test@example.com", password: "123", full_name: "Dawei Dai")
 
-Review.create!(user: dawei, video: yesman, rating: 5, content: "This is a really good movie!" )
-Review.create!(user: dawei, video: yesman, rating: 2, content: "This movie is not so good!")
+Review.create!(user: dawei, video: monk, rating: 5, content: "This is a really good movie!" )
+Review.create!(user: dawei, video: futurama, rating: 2, content: "This movie is not so good!")
+
+QueueItem.create!(user: dawei, video: monk)
+QueueItem.create!(user: dawei, video: futurama)
+QueueItem.create!(user: dawei, video: south_park)
